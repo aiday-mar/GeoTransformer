a78898241a09b23626810498c7a15d3dfc8953b2
aiday-mar <aidaymarlenkyzy@gmail.com>
2022-10-23 23:11:01 +0200

    removing certain comments, printing the value of k

--- geotransformer/modules/geotransformer/geotransformer.py
+++ geotransformer/modules/geotransformer/geotransformer.py
@@ -39,6 +39,7 @@ class GeometricStructureEmbedding(nn.Module):
         d_indices = dist_map / self.sigma_d
 
         k = self.angle_k
+        print('k : ', k)
         knn_indices = dist_map.topk(k=k + 1, dim=2, largest=False)[1][:, :, 1:]  # (B, N, k)
         knn_indices = knn_indices.unsqueeze(3).expand(batch_size, num_point, k, 3)  # (B, N, k, 3)
         expanded_points = points.unsqueeze(1).expand(batch_size, num_point, num_point, 3)  # (B, N, N, 3)
