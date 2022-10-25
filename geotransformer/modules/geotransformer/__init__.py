78a96b94bd8c7c3af64456d51116bc4763538891
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-10-25 18:56:08 +0200

    adding into init py

--- geotransformer/modules/geotransformer/__init__.py
+++ geotransformer/modules/geotransformer/__init__.py
@@ -2,4 +2,4 @@ from geotransformer.modules.geotransformer.geotransformer import GeometricStruct
 from geotransformer.modules.geotransformer.superpoint_matching import SuperPointMatching
 from geotransformer.modules.geotransformer.superpoint_target import SuperPointTargetGenerator
 from geotransformer.modules.geotransformer.point_matching import PointMatching
-from geotransformer.modules.geotransformer.local_global_registration import LocalGlobalRegistration
+from geotransformer.modules.geotransformer.local_global_registration import LocalGlobalRegistration, AstrivisLocalGlobalRegistration
