0391846885e44ca1d25b166658fb4cd974918e23
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-10-26 11:31:38 +0200

    setting number of stages to 2

--- experiments/geotransformer.modelnet.rpmnet.stage4.gse.k3.max.oacl.stage2.sinkhorn/config.py
+++ experiments/geotransformer.modelnet.rpmnet.stage4.gse.k3.max.oacl.stage2.sinkhorn/config.py
@@ -80,7 +80,7 @@ _C.optim.grad_acc_steps = 1
 
 # model - backbone
 _C.backbone = edict()
-_C.backbone.num_stages = 1 # seems to be correlated with number of super points
+_C.backbone.num_stages = 2 # seems to be correlated with number of super points
 _C.backbone.init_voxel_size = 0.05
 _C.backbone.kernel_size = 15
 _C.backbone.base_radius = 0.1 # used to be 2.5?
