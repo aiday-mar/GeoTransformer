dd75253fb294f50bcb41f1f21a2ee1690808872c
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-10-26 21:14:29 +0200

    printing a specific value

--- geotransformer/modules/geotransformer/local_global_registration.py
+++ geotransformer/modules/geotransformer/local_global_registration.py
@@ -467,6 +467,7 @@ class AstrivisLocalGlobalRegistration(nn.Module):
             print('batch_corr_residuals.shape : ', batch_corr_residuals.shape)
             optimal_transformations_per_superpoint = torch.min(batch_corr_residuals, dim=0, keepdim=False)
             print('optimal_transformations_per_superpoint.values : ', optimal_transformations_per_superpoint.values)
+            print('optimal_transformations_per_superpoint.values[0] : ', optimal_transformations_per_superpoint.values[0])
             print('optimal_transformations_per_superpoint.indices : ', optimal_transformations_per_superpoint.indices)
         
             batch_inlier_masks = torch.lt(batch_corr_residuals, self.acceptance_radius)  # (P, N)
