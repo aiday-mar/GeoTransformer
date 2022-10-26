0efd54b2ce5e45470dc28d3b31cd195e92e1cb52
aiday-mar <aidaymarlenkyzy@gmail.com>
2022-10-27 01:31:49 +0200

    rewriting the sh file

--- experiments/geotransformer.modelnet.rpmnet.stage4.gse.k3.max.oacl.stage2.sinkhorn/test_dynamic_motion_astrivis.sh
+++ experiments/geotransformer.modelnet.rpmnet.stage4.gse.k3.max.oacl.stage2.sinkhorn/test_dynamic_motion_astrivis.sh
@@ -7,7 +7,7 @@ for i in 005 018 036 053 057 068 070 082 094 100 101 113 116 141 166 175; do
 
 echo $i
 cd model-${i}-sampled005
-    rm -v !("dense1.ply"|"dense2.ply")
+    rm !(dense1.ply|dense2.ply)
 cd ..
 
 python astrivis-test-multiple-transforms.py --source="model-${i}-sampled005/dense1.ply" --target="model-${i}-sampled005/dense2.ply" --output="model-${i}-sampled005/output.ply" --weights="../../output/geotransformer.modelnet.rpmnet.stage4.gse.k3.max.oacl.stage2.sinkhorn/snapshots/model-39.pth.tar" --directory="model-${i}-sampled005"
