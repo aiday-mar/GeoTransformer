be1213137dc102bc6246d852e8407c654c2ed2e2
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-10-27 11:43:49 +0200

    removing the printing of values and indices of optimal_transformations_per_superpoint

--- geotransformer/modules/geotransformer/local_global_registration.py
+++ geotransformer/modules/geotransformer/local_global_registration.py
@@ -454,8 +454,8 @@ class AstrivisLocalGlobalRegistration(nn.Module):
             print('batch_corr_residuals.shape : ', batch_corr_residuals.shape)
             optimal_transformations_per_superpoint = torch.min(batch_corr_residuals, dim=0, keepdim=False)
             optimal_transformations_per_superpoint = {'values' : optimal_transformations_per_superpoint.values, 'indices' : optimal_transformations_per_superpoint.indices}
-            print('optimal_transformations_per_superpoint.values : ', optimal_transformations_per_superpoint['values'])
-            print('optimal_transformations_per_superpoint.indices : ', optimal_transformations_per_superpoint['indices'])
+            # print('optimal_transformations_per_superpoint.values : ', optimal_transformations_per_superpoint['values'])
+            # print('optimal_transformations_per_superpoint.indices : ', optimal_transformations_per_superpoint['indices'])
         
             batch_inlier_masks = torch.lt(batch_corr_residuals, self.acceptance_radius)  # (P, N)
             print('batch_inlier_masks.shape : ', batch_inlier_masks.shape)
