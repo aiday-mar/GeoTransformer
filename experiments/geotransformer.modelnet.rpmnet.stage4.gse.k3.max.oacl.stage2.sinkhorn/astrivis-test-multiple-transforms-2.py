351cc08fd84e3ea96d45fbc25ace8d7f8e67afed
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-10-27 16:22:34 +0200

    appending to the array

--- experiments/geotransformer.modelnet.rpmnet.stage4.gse.k3.max.oacl.stage2.sinkhorn/astrivis-test-multiple-transforms-2.py
+++ experiments/geotransformer.modelnet.rpmnet.stage4.gse.k3.max.oacl.stage2.sinkhorn/astrivis-test-multiple-transforms-2.py
@@ -101,7 +101,7 @@ def main():
     transform_to_superpoint = {}
     copy_superpoint_src_corr_points = superpoint_src_corr_points
     copy_superpoint_ref_corr_points = superpoint_ref_corr_points
-    transformed_superpoints_pcd = []
+    transformed_superpoints_pcd = np.array([])
     n_rows = 0
     rotation_n = 0
     
@@ -133,7 +133,7 @@ def main():
         
         transformed_inliers = apply_transform(torch.tensor(copy_superpoint_src_corr_points[indices_inliers]), torch.tensor(transform))
         print('transformed_inliers.shape : ', transformed_inliers.shape)
-        transformed_superpoints_pcd.append(transformed_inliers.tolist())
+        transformed_superpoints_pcd = np.append(transformed_superpoints_pcd, np.array(transformed_inliers), axis=0)
         print('transformed_superpoints_pcd : ', transformed_superpoints_pcd)
         break
     
