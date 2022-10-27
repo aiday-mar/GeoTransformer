8cde34f6d763e6418441be71003f3c2da0a601c6
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-10-27 15:33:31 +0200

    accessing the acceptance radius inside of the config variable

--- experiments/geotransformer.modelnet.rpmnet.stage4.gse.k3.max.oacl.stage2.sinkhorn/astrivis-test-multiple-transforms-2.py
+++ experiments/geotransformer.modelnet.rpmnet.stage4.gse.k3.max.oacl.stage2.sinkhorn/astrivis-test-multiple-transforms-2.py
@@ -110,11 +110,11 @@ def main():
         transformed_src_superpoints = apply_transform(torch.tensor(copy_superpoint_src_corr_points), torch.tensor(transform))
         print('transformed_src_superpoints.shape : ', transformed_src_superpoints.shape)
         residual = torch.linalg.norm(
-            torch.tensor(superpoint_ref_corr_points) - torch.tensor(transformed_src_superpoints), dim=1
+            torch.tensor(superpoint_ref_corr_points) - transformed_src_superpoints, dim=1
         )
         print('residual.shape : ', residual.shape)
-        batch_inlier_masks = torch.lt(residual, args.acceptance_radius)
-        batch_outlier_masks = torch.gt(residual, args.acceptance_radius)
+        batch_inlier_masks = torch.lt(residual, cfg.fine_matching.acceptance_radius.acceptance_radius)
+        batch_outlier_masks = torch.gt(residual, cfg.fine_matching.acceptance_radius.acceptance_radius)
         print('batch_inlier_masks.shape : ', batch_inlier_masks.shape)
         print('batch_outlier_masks.shape : ', batch_inlier_masks.shape)
         indices_inliers = batch_inlier_masks.nonzero()
