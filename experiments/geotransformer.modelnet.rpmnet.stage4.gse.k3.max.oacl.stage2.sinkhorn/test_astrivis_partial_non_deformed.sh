5fb136b98cb339e946da575e579980fe65cf07f7
aiday-mar <aidaymarlenkyzy@gmail.com>
2023-01-24 17:33:37 +0100

    we have one model for 0.03

--- experiments/geotransformer.modelnet.rpmnet.stage4.gse.k3.max.oacl.stage2.sinkhorn/test_astrivis_partial_non_deformed.sh
+++ experiments/geotransformer.modelnet.rpmnet.stage4.gse.k3.max.oacl.stage2.sinkhorn/test_astrivis_partial_non_deformed.sh
@@ -1,12 +1,12 @@
 base='/home/aiday.kyzy/dataset/Synthetic/PartialNonDeformedData/TestingData'
 
-# one_model=True
-one_model=False
+one_model=True
+# one_model=False
 
-model_numbers=('002' '042' '085' '126' '167' '207')
-# model_numbers=('126')
+# model_numbers=('002' '042' '085' '126' '167' '207')
+model_numbers=('002')
 
-init_voxel_size=0.01
+init_voxel_size=0.03
 
 # training_data='pretrained'
 training_data='partial_non_deformed'
