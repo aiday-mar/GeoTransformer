24e4e628ae4a036c6f225db6829938462e6e3614
aiday-mar <aidaymarlenkyzy@gmail.com>
2023-01-25 14:23:39 +0100

    decreasing the voxel size

--- experiments/geotransformer.modelnet.rpmnet.stage4.gse.k3.max.oacl.stage2.sinkhorn/test_astrivis_full_deformed.sh
+++ experiments/geotransformer.modelnet.rpmnet.stage4.gse.k3.max.oacl.stage2.sinkhorn/test_astrivis_full_deformed.sh
@@ -7,7 +7,7 @@ training_data='full_non_deformed'
 current_deformation=True
 # current_deformation=False
 
-init_voxel_size=0.02
+init_voxel_size=0.005
 
 # weights="geotransformer-modelnet.pth.tar"
 weights="model_320_full_non_deformed.pth.tar"
